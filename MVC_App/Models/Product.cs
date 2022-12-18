5827729464622eb74e5cbc64ac8d33009cdd857b
Arumilli Tanuj Kumar <Arumilli.Tanuj.Kumar@gds.ey.com>
2022-12-18 16:19:09 +0530

    Add project files.

---
+++ MVC_App/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC_App.Models
+{
+    public class Product
+    {
+        public int prodId { get; set; }
+        public string prodName { get; set; }
+        public int quantity { get; set; }
+    }
+}
---
+++ MVC_App/Repositories/ProductsRepo.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using MVC_App.Models;
+using System.Data.SqlClient;
+
+namespace MVC_App.Repositories
+{
+    public class ProductsRepo
+    {
+         
+        public List<Product> GetProducts()
+        {
+            try
+            {
+                using (var connection = new SqlConnection("data source=tanujsqldatabase.database.windows.net; database= appdb; user id =admin123; password = intelCOREi78thGen"))
+                {
+                    var productsLst = connection.Query<Product>("select * from tblproducts").ToList();
+                    return productsLst;
+                }
+            }
+
+
+
+            catch (Exception)
+            {
+
+                throw;
+                
+            }
+        }
+    }
+}
